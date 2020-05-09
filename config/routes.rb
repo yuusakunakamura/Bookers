c4466acf0774898a1acf3c2f017eaa241c48bcff
vagrant <vagrant@localhost.localdomain>
2020-05-09 08:35:20 +0000

    Initial Commit Message

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+
+  
+ root :to => 'homes#top'
+ get "books/new"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ post 'books' => 'books#create'
+
+ get 'books' => 'books#index'
+
+ get 'books/:id' => 'books#show', as: 'book'
+
+ get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+
+ patch 'books/:id' => 'books#update', as: 'update_book'
+
+ delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+   
+end
+
+
+
